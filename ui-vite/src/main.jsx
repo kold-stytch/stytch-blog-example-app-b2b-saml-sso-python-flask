701756d2a295089fad5704a672f95a41d28347bb
Kevin Old <kold@stytch.com>
2024-07-17 15:46:03 -0500

    configure react router dom

--- ui-vite/src/main.jsx
+++ ui-vite/src/main.jsx
@@ -5,6 +5,7 @@ import './index.css';
 
 import { StytchB2BProvider } from '@stytch/react/b2b';
 import { StytchB2BHeadlessClient } from '@stytch/vanilla-js/b2b/headless';
+import { BrowserRouter } from 'react-router-dom';
 
 const stytch = new StytchB2BHeadlessClient(
   import.meta.env.VITE_STYTCH_PUBLIC_TOKEN, // or process.env.STYTCH_PUBLIC_TOKEN for non-Vite based projects
@@ -12,8 +13,10 @@ const stytch = new StytchB2BHeadlessClient(
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <BrowserRouter>
     <StytchB2BProvider stytch={stytch}>
       <App />
     </StytchB2BProvider>
+    </BrowserRouter>
   </React.StrictMode>
 )
