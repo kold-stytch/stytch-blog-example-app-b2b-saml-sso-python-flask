c098d7e31b4556d3e6c1de94c53325cc3c0df238
Kevin Old <kold@stytch.com>
2024-07-17 15:13:37 -0500

    configure stytch for the FE and get member

--- ui-vite/src/main.jsx
+++ ui-vite/src/main.jsx
@@ -1,10 +1,19 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App.jsx';
+import './index.css';
+
+import { StytchB2BProvider } from '@stytch/react/b2b';
+import { StytchB2BHeadlessClient } from '@stytch/vanilla-js/b2b/headless';
+
+const stytch = new StytchB2BHeadlessClient(
+  import.meta.env.VITE_STYTCH_PUBLIC_TOKEN, // or process.env.STYTCH_PUBLIC_TOKEN for non-Vite based projects
+);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+    <StytchB2BProvider stytch={stytch}>
+      <App />
+    </StytchB2BProvider>
+  </React.StrictMode>
 )
