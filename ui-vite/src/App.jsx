edff84e96d394f451da95a4e59507b05f2965e65
Kevin Old <kold@stytch.com>
2024-07-17 16:35:04 -0500

    move to components

--- ui-vite/src/App.jsx
+++ ui-vite/src/App.jsx
@@ -1,48 +1,11 @@
 import { Button, Container } from '@mui/material';
 import { useStytchB2BClient, useStytchMemberSession } from '@stytch/react/b2b';
-import React, { useCallback, useEffect, useState } from 'react';
-import { Route, Routes, useParams } from 'react-router-dom';
+import React, { useCallback } from 'react';
+import { Route, Routes } from 'react-router-dom';
 import './App.css';
+import Home from './Home';
 import ProductsList from './ProductsList';
-
-function B2BLogin() {
-  const { orgId } = useParams();
-  const [orgName, setOrgName] = useState('');
-  const [orgUrl, setOrgUrl] = useState('');
-
-  useEffect(() => {
-    fetch(`http://localhost:3000/org/${orgId}`)
-      .then(res => res.json())
-      .then(data => {
-        console.log(data)
-        setOrgName(data.org_name)
-        setOrgUrl(data.sso_url)
-      })
-      .catch(error => console.error('Error fetching org data:', error));
-  }, [orgId]);
-
-  return (
-    <div className="login-container">
-      <div className="login-card">
-        <h2 className="login-title">
-          Continue to {orgName}
-        </h2>
-        <a href={orgUrl} className="login-link">
-          Continue with SAML SSO
-        </a>
-      </div>
-    </div>
-  );
-}
-
-function Home() {
-  return (
-    <>
-      <h3>Visit /org/:orgId to continue.</h3>
-      <h3>Replace orgId with your organization ID from the Stytch Dashboard (e.g. /org/test-org)</h3>
-    </>
-  );
-}
+import SSOLogin from './SSOLogin';
 
 function App() {
   const stytch = useStytchB2BClient();
@@ -68,7 +31,7 @@ function App() {
       )}
 
       <Routes>
-        <Route path="/org/:orgId" element={<B2BLogin />} />
+        <Route path="/org/:orgId" element={<SSOLogin />} />
         <Route path="/" element={session || fromCache ? <ProductsList /> : <Home />} />
       </Routes>
     </Container>
