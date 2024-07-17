f1c13729a6474f44e7b02b5120692b492e7bed99
Kevin Old <kold@stytch.com>
2024-07-17 16:48:17 -0500

    cleanup code

--- api/app.py
+++ api/app.py
@@ -1,7 +1,7 @@
 import os
 import sys
 
-from flask import Flask, request, session, redirect, make_response
+from flask import Flask, request, redirect, make_response
 from flask_cors import CORS
 from stytch import B2BClient
 
@@ -52,8 +52,7 @@ def authenticate():
         # handle other authentication method callbacks
         return "unsupported auth method"
 
-    response.set_cookie("stytch_session", resp.session_token, max_age=60 * 60 * 24)
-    response.set_cookie("stytch_session_jwt", resp.session_jwt, max_age=60 * 60 * 24)
+    response.set_cookie("stytch_session", resp.session_token)
     return response
 
 
@@ -80,53 +79,3 @@ def org_index(slug: str):
 if __name__ == "__main__":
     # in production you would want to make sure to disable debugging
     app.run(host=HOST, port=PORT, debug=True)
-
-# @app.route("/authenticate", methods=["POST"])
-# def authenticate():
-#     token = request.json.get("token")
-#     tokenType = request.json.get("type")
-#     try:
-#         # if tokenType == "sso":
-#         resp = client.sso.authenticate(sso_token=token)
-#         return handle_authenticate_response(resp), 200
-#     # else:
-#     #     resp = client.magic_links.authenticate(magic_links_token=token)
-#     #     return handle_authenticate_response(resp), 200
-#     except Exception as e:
-#         return jsonify(error=str(e)), 500
-
-# def handle_authenticate_response(resp):
-#     # Basic info
-#     status_code = resp.status_code
-#     request_id = resp.request_id
-#     # Member info
-#     member_info = {
-#         "member_id": resp.member.member_id,
-#         "email_address": resp.member.email_address,
-#         "status": resp.member.status,
-#         "name": resp.member.name,
-#         "is_admin": resp.member.is_admin,
-#     }
-
-#     # Session info
-#     session_token = resp.session_token
-#     session_jwt = resp.session_jwt
-
-#     # Organization info
-#     organization_info = {
-#         "organization_id": resp.organization.organization_id,
-#         "organization_name": resp.organization.organization_name,
-#         "organization_slug": resp.organization.organization_slug,
-#     }
-
-#     # Constructing a response dict
-#     response_data = {
-#         "status_code": status_code,
-#         "request_id": request_id,
-#         "member_info": member_info,
-#         "session_token": session_token,
-#         "session_jwt": session_jwt,
-#         "organization_info": organization_info,
-#     }
-
-#     return response_data
